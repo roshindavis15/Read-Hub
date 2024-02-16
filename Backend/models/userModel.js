b3e3d589b0a185ed87348c79ab17df77c385c569
roshin <roshindavis15@gmail.com>
2024-02-16 10:53:03 +0530

    user model created and connected to mongodb

---
+++ Backend/models/userModel.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const userModel = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true, 
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    mobile: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true, 
+    },
+    is_admin: {
+        type: Boolean,
+        default: false
+    },
+    isVarified: {
+        type: Boolean,
+        default: false
+    },
+});
+
+module.exports = mongoose.model('User', userModel);
--- Backend/server.js
+++ Backend/server.js
@@ -1,4 +1,6 @@
+require('dotenv').config();
 const express=require('express');
+const mongoose =require('mongoose')
 
 const app=express();
 
@@ -6,3 +8,11 @@ const PORT=process.env.PORT || 3000;
 app.listen(PORT,()=>{
     console.log(`Server is running on the port ${PORT}`);
 })
+
+
+mongoose.connect(process.env.DATABASE_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(()=>console.log("MongoDB connecteed"))
+.catch(err=> console.error("Error connecting to MongoDB:",err));
